6dd624f0745c5936d5b0e60032167e4922ebe1a0
Jay McMullen <j@1f.io>
2025-05-21 09:55:57 +1000

    feat: desktop designs

--- src/components/ui/Features.tsx
+++ src/components/ui/Features.tsx
@@ -1,6 +1,7 @@
 import iconBrandRecognition from "~/assets/images/icon-brand-recognition.svg";
 import iconDetailedRecords from "~/assets/images/icon-detailed-records.svg";
 import iconFullyCustomizable from "~/assets/images/icon-fully-customizable.svg";
+import { cn } from "~/utils/cn";
 
 const features = [
   {
@@ -27,30 +28,45 @@ const features = [
 ];
 
 export const Features: React.FC = () => {
+  const featureVariants = ["md:mt-0", "md:mt-12", "md:mt-24"];
+  const connectorVariants = ["", "md:top-[120px]", "md:top-[70px]"];
+
   return (
-    <section className="flex flex-col gap-y-8 px-6 bg-light-gray pb-28 pt-44 -mt-20">
-      <div className="text-center">
-        <h2 className="text-2xl font-bold mb-6">Advanced Statistics</h2>
-        <p className="text-grayish-violet">
-          Track how your links are performing across the web with our advanced
-          statistics dashboard.
-        </p>
+    <section className="bg-light-gray pb-28 pt-44 -mt-20">
+      <div className="flex flex-col gap-y-8 px-6 max-w-7xl mx-auto">
+        <div className="text-center">
+          <h2 className="text-2xl font-bold mb-6">Advanced Statistics</h2>
+          <p className="text-grayish-violet">
+            Track how your links are performing across the web with our advanced
+            statistics dashboard.
+          </p>
+        </div>
+        <ul className="flex flex-col md:flex-row md:gap-x-8 gap-y-24 mt-20">
+          {features.map((feature, index) => (
+            <li
+              key={feature.id}
+              className={cn(
+                "w-full p-6 pt-20 bg-white rounded-md text-center md:text-left flex flex-col gap-y-4 items-center relative self-start md:justify-start",
+                featureVariants[index]
+              )}
+            >
+              {index > 0 && (
+                <div
+                  className={cn(
+                    "absolute h-20 w-2 -top-24 bg-cyan md:w-8 md:h-2 md:-left-8",
+                    connectorVariants[index]
+                  )}
+                />
+              )}
+              <div className="bg-dark-violet size-20 rounded-full flex items-center justify-center p-3 absolute -top-10 md:self-start">
+                <img src={feature.icon} alt={feature.title} />
+              </div>
+              <h3 className="text-xl font-bold w-full">{feature.title}</h3>
+              <p className="text-grayish-violet">{feature.description}</p>
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul className="flex flex-col gap-y-24 mt-20">
-        {features.map((feature, index) => (
-          <li
-            key={feature.id}
-            className="p-6 pt-20 bg-white rounded-md text-center flex flex-col gap-y-4 items-center relative"
-          >
-            {index > 0 && <div className="absolute h-20 w-2 -top-24 bg-cyan" />}
-            <div className="bg-dark-violet size-20 rounded-full flex items-center justify-center p-3 absolute -top-10">
-              <img src={feature.icon} alt={feature.title} />
-            </div>
-            <h3 className="text-xl font-bold">{feature.title}</h3>
-            <p className="text-grayish-violet">{feature.description}</p>
-          </li>
-        ))}
-      </ul>
     </section>
   );
 };
--- src/components/ui/Shorten.tsx
+++ src/components/ui/Shorten.tsx
@@ -2,11 +2,11 @@ import { Button } from "./Button";
 
 export const Shorten: React.FC = () => {
   return (
-    <form className="flex flex-col gap-y-4 bg-dark-violet p-4 rounded-md w-full bg- bg-shorten-mobile md:bg-shorten-desktop bg-no-repeat bg-right-top">
+    <form className="flex flex-col md:flex-row gap-y-4 md:gap-x-6 bg-dark-violet p-4 md:p-16 rounded-md w-full bg-shorten-mobile md:bg-shorten-desktop bg-no-repeat bg-right-top md:bg-cover">
       <input
         type="text"
         placeholder="Shorten a link here..."
-        className="p-4 border border-grayish-violet rounded-md"
+        className="p-4 border flex-1 border-grayish-violet rounded-md"
       />
       <Button type="submit" rounded={false}>
         Shorten It!
